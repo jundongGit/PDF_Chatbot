1111a9f1c70db14fb7a8443968612dc403d62f01
jundongGit <41845422+jundongGit@users.noreply.github.com>
2023-07-16 21:26:07 +1200

    Add files via upload

---
+++ app.py
@@ -0,0 +1,48 @@
+import os
+import streamlit as st
+from langchain.document_loaders import PyMuPDFLoader
+from langchain.text_splitter import RecursiveCharacterTextSplitter
+from langchain.embeddings.openai import OpenAIEmbeddings
+from langchain.vectorstores import Chroma
+from langchain.llms import OpenAI
+from langchain. chains.question_answering import load_qa_chain
+from dotenv import load_dotenv
+
+load_dotenv()
+
+OPENAI_API_KEY = os.getenv("OPENAI_API_KEY")
+embeddings = OpenAIEmbeddings(openai_api_key=OPENAI_API_KEY)
+persist_directory = 'pdf_persist'
+collection_name = 'pdf_collection'
+llm = OpenAI (temperature=0, openai_api_key=OPENAI_API_KEY)
+chain = load_qa_chain(llm, chain_type="stuff")
+vectorstore = None
+
+def load_pdf (pdf_path):
+ return PyMuPDFLoader (pdf_path).load ()
+
+st.title("PDF Chatbot")
+
+with st.container():
+ uploaded_file = st.file_uploader("Choose a PDF file", type="pdf")
+ if uploaded_file is not None:
+  path = os.path.join('.',uploaded_file.name)
+  with open(path, 'wb') as f:
+   f.write(uploaded_file.getbuffer())
+
+   docs = load_pdf(path)
+
+   text_splitter = RecursiveCharacterTextSplitter(chunk_size=1000, chunk_overlap=200)
+   split_doc = text_splitter.split_documents(docs)
+
+   vectorstore = Chroma.from_documents(split_doc, embeddings, collection_name=collection_name, persist_directory=persist_directory)
+   vectorstore.persist()
+
+   st.write("Done")
+
+   with st.container():
+    question = st.text_input("Question")
+    if vectorstore is not None and question is not None and question != "":
+     docs = vectorstore.similarity_search(question, 3, include_metadata=True)
+     answer = chain.run(input_documents=docs, question=question)
+     st.write(answer)
